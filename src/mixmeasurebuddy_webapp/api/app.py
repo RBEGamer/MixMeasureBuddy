55a326b5b55526d5822cb62c9fbbb218b1cb66d2
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-07-29 03:29:53 +0200

    .

--- src/mixmeasurebuddy_webapp/api/app.py
+++ src/mixmeasurebuddy_webapp/api/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, jsonify, make_response, Blueprint, redirect
 from dotenv import load_dotenv
 import os
@@ -5,9 +7,11 @@ from pathlib import Path
 from flask_swagger_generator.components import SwaggerVersion
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_swagger_generator.generators import Generator
-from flask_swagger_generator.utils import SecurityType
 import logging
 from configparser import ConfigParser
+import pymongo
+import mongoengine
+import names
 # IMPORT CUSTOM DATABASE MODELS
 import dbmodels
 
@@ -17,7 +21,7 @@ load_dotenv() # LOAD .env
 config = ConfigParser()
 config.read(Path.joinpath(Path(__file__).parent, Path("config.cfg")))
 
-SWAGGER_URL = '/docs'  # URL for exposing Swagger UI (without trailing '/')
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 SWAGGERFILE_PATH = 'static/swagger.yaml'
 SWAGGERFILE_PATH_ABS = Path.joinpath(Path(__file__).parent, Path(SWAGGERFILE_PATH))
 
@@ -36,21 +40,45 @@ app.register_blueprint(get_swaggerui_blueprint(SWAGGER_URL, "/" + SWAGGERFILE_PA
 
 
 
+@blueprint.route('/api/mmb/', methods=['GET'])
+def mmbd_index(mmb_device_id: str):  # mixmeasurebuddy.com/api/ system_id / recipes.json
+    return make_response(jsonify({'mmbd_recipes': '/api/mmb/<string:mmb_device_id>', 'mmbd_recipe': '/api/mmb/<string:mmb_device_id>/<string:recipe_id>'}), 200)
 
 
-# SPECIFI GENERATED FOR DEVICES
+
+
+
 # ["resipe_file_uri_relative"]
 @generator.response(status_code=200, schema={'id': 10, 'name': 'test_object'})
-@blueprint.route('/mmb/<string:mmb_device_id>', methods=['GET'])
+@blueprint.route('/api/mmb/<string:mmb_device_id>/recipes', methods=['GET'])
 def mmbd_recipes(mmb_device_id: str):  # mixmeasurebuddy.com/api/ system_id / recipes.json
+
+    # CREATE USER IF NOT PRESENT
+
+    if len(dbmodels.Users.objects(linked_device_id=mmb_device_id)) <= 0:
+        user = dbmodels.Users(name="", linked_device_id=mmb_device_id)
+        user.save()
+
+
     data: list = [
         "Tequila_Sunrise",
     ]
     return make_response(jsonify(data), 200)
 
 @generator.response(status_code=200, schema={'recipe': {'name': 'Tequila_Sunrise', 'description': 'recipe text','version': '1.0.0','ingredients': {'0': 'name'},'steps':[{'action':'scale', 'ingredient':'0', 'amount':720},{'action':'confirm', 'text':'press ok'}]}, 'name': 'Recipe_Name'})
-@blueprint.route('/mmb/<string:mmb_device_id>/<string:recipe_id>', methods=['GET'])
+@blueprint.route('/api/mmb/<string:mmb_device_id>/recipe/<string:recipe_id>', methods=['GET'])
 def mmbd_recipe(mmb_device_id: str, recipe_id: str):
+
+    # TODO ON RP2040 CHECK FOR VALID RESULT
+    # CREATE USER IF NOT PRESENT
+    if len(dbmodels.Users.objects(linked_device_id=mmb_device_id)) <= 0:
+        user = dbmodels.Users(name="", linked_device_id=mmb_device_id)
+        user.save()
+
+
+
+
+
     data: dict = {
 
 
@@ -61,14 +89,87 @@ def mmbd_recipe(mmb_device_id: str, recipe_id: str):
 
 
 
-@blueprint.route('/')
-def index():
+@blueprint.route('/api')
+def api():
     return redirect(SWAGGER_URL)
 
-
+@blueprint.route('/')
+def index():
+    return redirect('/api')
 
 app.register_blueprint(blueprint)
 generator.generate_swagger(app, destination_path=SWAGGERFILE_PATH)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("API_SERVER_PORT", 9090), debug=True)
+    # CHECK DB CONNECTION
+    DATABASE_CONNECTION_STRING: str = os.environ.get("DATABASE_CONNECTION_STRING", "mongodb://mongo:27017")
+    DB_NAME: str = os.environ.get("MMB_DATABASE_NAME", "mixmeasurebuddy")
+    DB_COLLECTION_RECIPES: str = "recipes"
+    DB_COLLECTION_USERS: str = "users"
+    DB_COLLECTION_INGREDIENTS: str = "ingredients"
+
+    # PREPARE DATABASE
+    # WE ARE USING PYMONGO TO CHECK THE DATABASE
+    db: pymongo.MongoClient = pymongo.MongoClient(DATABASE_CONNECTION_STRING)
+    # CREATE DATABASE
+    dblist = db.list_database_names()
+
+    if DB_NAME in dblist:
+        logging.debug("The MMB_DATABASE {} exists.".format(DB_NAME))
+    mmb_database = db[DB_NAME]
+    # CREATE COLLECTIONS
+    target_collections = [DB_COLLECTION_RECIPES, DB_COLLECTION_USERS, DB_COLLECTION_INGREDIENTS]
+    existing_collections = mmb_database.list_collection_names()
+    for collection in target_collections:
+        if collection in existing_collections:
+            logging.debug("The collection {} exists.".format(collection))
+        else:
+            mmb_database.create_collection(collection)
+    # FINALLY CLOSE CONNECTION
+    db.close()
+
+
+    # CREATE mongoengine CONNECTION
+    cs = "{}/{}".format(DATABASE_CONNECTION_STRING, DB_NAME)
+    logging.debug(cs)
+
+
+    mongoengine.connect(host=cs)
+
+
+
+    test_ing = [
+        "Pineapple-Juice",
+        "Cream",
+        "white Rum",
+        "Crushed Ice",
+        "Coconut-Juice",
+        "Strawberries",
+        "white Tequila",
+        "Grenadine",
+        "Orange-Juice"
+    ]
+
+    for i in test_ing:
+        if len(dbmodels.Ingredient.objects(name=i)) <= 0:
+            r = dbmodels.Ingredient()
+            r.name = i
+            r.save()
+
+    if len(dbmodels.Recipe.objects(name="Tequila Sunrise")) <= 0:
+        i1 = dbmodels.Ingredient.objects(name="white Tequila")
+
+        r = dbmodels.Recipe()
+        r.name = "Tequila Sunrise"
+        r.description = "A nice Tequila Sunrise Cocktail"
+        r.ingredients = [i1[0]]
+        r.steps = [dbmodels.Step(amount=10, action="scale", ingredient=[i1])]
+        r.save()
+
+    #    r1.ingredients["0"] = "10 Strawberries"
+
+        #r1.steps = [dbmodels.Step(text="puree strawberries", action="confirm")]
+
+        #r1.save()
+    # START FLASK
+    app.run(host=os.environ.get("API_SERVER_BIND_ADDR", '0.0.0.0'), port=os.environ.get("API_SERVER_PORT", 5000), debug=True)
--- src/mixmeasurebuddy_webapp/api/dbmodels.py
+++ src/mixmeasurebuddy_webapp/api/dbmodels.py
@@ -1,24 +1,64 @@
-from mongoengine import Document
-from mongoengine import DateTimeField, StringField, ReferenceField, ListField
+from datetime import datetime
+import names
+from mongoengine import Document, IntField
+from mongoengine import DateTimeField, StringField, ReferenceField, ListField, DictField, FloatField
 
 
 class Ingredient(Document):
+    name = StringField(required=True, unique=True)
+    weight_g_per_unit = FloatField(default=1.0, required=True)
 
-    name = StringField(max_length=20, required=True, unique=False)
-    description = StringField(max_length=60, required=False, unique=False)
     def __unicode__(self):
         return self.name
 
     def __repr__(self):
         return self.name
+
+
+    meta = {'collection': 'ingredients'}
+
+class Step(Document):
+    action = StringField(required=True)
+    text = StringField(required=False)
+    ingredient = ListField[Ingredient]
+    amount = IntField(required=False)
+
+    def __unicode__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    meta = {'collection': 'recipes'}
+
 class Recipe(Document):
     name = StringField(max_length=20, required=True, unique=True)
-    description = StringField(max_length=60, required=True, unique=True)
-    version = StringField(max_length=10, required=True, unique=True)
-    ingredients = ListField(ReferenceField(Ingredient))
+    description = StringField(default="A nice new Cocktail", required=True)
+    version = StringField(default="1.0.0", required=True)
+    ingredients = ListField(Ingredient, required=True)
+    steps = ListField(Step, required=True)
+    def __unicode__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    meta = {'collection': 'recipes'}
+
+
+
+
+class Users(Document):
+    name: StringField = StringField(default=names.get_full_name())
+    linked_device_id: StringField = StringField()
+    date_modified = DateTimeField(default=datetime.utcnow)
+    linked_recipes: ListField(ReferenceField(Recipe))
+
 
     def __unicode__(self):
         return self.name
 
     def __repr__(self):
         return self.name
+
+    meta = {'collection': 'users'}
