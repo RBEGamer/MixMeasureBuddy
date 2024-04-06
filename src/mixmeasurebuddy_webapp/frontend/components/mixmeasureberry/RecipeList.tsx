b92855138a1806ca1d5e658137253aa68182d1ec
Hack4Pott Team 10 <marcel.ochsendorf@gmail.com>
2024-04-06 14:42:36 +0200

    Update RecipeList.tsx

--- src/mixmeasurebuddy_webapp/frontend/components/mixmeasureberry/RecipeList.tsx
+++ src/mixmeasurebuddy_webapp/frontend/components/mixmeasureberry/RecipeList.tsx
@@ -1,4 +1,4 @@
-import {FeatureListItem, LandingFeatureList} from "@/components/landing/LandingFeatureList";
+import {FeatureListItem} from "@/components/landing/LandingFeatureList";
 import {
     GlassWater} from 'lucide-react';
 
@@ -8,17 +8,49 @@ import { cn } from '@/lib/utils';
 
 
 
+import clsx from 'clsx';
+import {alignments} from "@floating-ui/utils";
+
 /**
- * A component meant to be used in the landing page.
- * It displays a grid list of features.
+ * This component is meant to be used in the landing page, in the features list.
  *
- * Each feature has a title, description and icon.
+ * Describes a single feature, with a title, description and icon.
  */
+export const MMBLandingFeatureITEM = ({
+                                   className,
+                                   title,
+                                   description,
+                                   author,
+                                   icon,
+                               }: {
+    className?: string;
+    title: string;
+    description: string;
+    author: string;
+    icon: React.ReactNode;
+}) => {
+    return (
+        <div className={clsx('flex flex-col gap-4 py-4', className)}>
+
+                <div className="flex w-100  align-middle rounded-md bg-primary-100/30 border border-primary-100/70 dark:bg-primary-900/70">
+                    <div className={clsx('flex-col gap-6 py-6 left-0', className)}>{icon}</div>
+                    <div className={clsx('flex-col gap-6 py-6 right-0', className)}><h3
+                        className="font-semibold text-xl">{title}</h3></div>
+                    </div>
+
+
+                    <p className="text-sm text-gray-800 dark:text-gray-200">{description}</p>
+            <h4 className="text-small font-semibold">by {author}</h4>
+        </div>
+    );
+};
+
+
 export const MMBRecipeListVisual = ({
-                                       className,
-                                       title,
-                                       description,
-                                       featureItems,
+                                        className,
+                                        title,
+                                        description,
+                                        featureItems,
                                        withBackground = false,
                                    }: {
     className?: string;
@@ -43,9 +75,10 @@ export const MMBRecipeListVisual = ({
 
                 <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-12">
                     {featureItems.map((featureItem, index) => (
-                        <LandingFeature
+                        <MMBLandingFeatureITEM
                             key={index}
-                            title={featureItem.title}
+                            title={featureItem.name}
+                            author={featureItem.author}
                             description={featureItem.description}
                             icon=<GlassWater/>
                         />
