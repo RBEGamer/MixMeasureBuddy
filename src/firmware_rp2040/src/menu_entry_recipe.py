f946302940b4ea028cd69921e9540b50827ad681
Hack4Pott Team 10 <marcel.ochsendorf@gmail.com>
2024-04-22 13:59:28 +0200

    starting recipe state handling

--- src/firmware_rp2040/src/menu_entry_recipe.py
+++ src/firmware_rp2040/src/menu_entry_recipe.py
@@ -6,13 +6,21 @@ from ledring import ledring
 from menu_manager import menu_manager
 import recipe_loader
 import recipe
-
+import time
+from Scales import ScaleInterface
 class menu_entry_recipe(menu_entry.menu_entry):
+
+    RECIPE_STATE_OVERVIEW = 0
+    RECIPE_STATE_INITIAL_TARE = 1
+    RECIPE_RUNNING = 2
 
 
    
     recipe_filename: str = ""
     loaded_recipe: recipe.recipe = None
+    last_scale_value: float = 0
+    recipe_state: int = RECIPE_STATE_OVERVIEW
+
 
     def __init__(self, _recipe_filename: str, _recipe_name: str, _recipe_desciption: str):
         super().__init__(_recipe_name, _recipe_desciption)
@@ -39,10 +47,14 @@ class menu_entry_recipe(menu_entry.menu_entry):
 
 
         # SHOW RECIPE OVERVIEW PAGE
+        self.recipe_state = self.RECIPE_STATE_OVERVIEW
         ui().show_recipe_ingredients(self.loaded_recipe.get_ingredients_as_names_list(), True)
 
+        # TARE SCALE
+        ScaleInterface().tare()
 
-
+        # RESET RECIPE
+        self.loaded_recipe.reset_steps()
 
     def teardown(self):
         print("teardown {}".format(self.name))
@@ -52,10 +64,38 @@ class menu_entry_recipe(menu_entry.menu_entry):
             self.loaded_recipe = None
 
 
+    def tare_scale():
+            ScaleInterface().tare()
+            time.sleep(1)
+            ui().show_msg("-- TARE SCALE --")
+            ScaleInterface().tare()
+
 
     def update(self, _system_command: system_command.system_command):
         if self.loaded_recipe is None:
             menu_manager.exit_current_menu()
+
+        # IF USER IS IN OVERVIEW SCREEN SWITCH TO FIRST RECIPE STEP
+        if self.recipe_state == self.RECIPE_STATE_OVERVIEW:
+            if _system_command.type == system_command.system_command.COMMAND_TYPE_NAVIGATION:
+                ui().show_msg("Please place glass and press a button")
+                self.recipe_state = self.RECIPE_STATE_INITIAL_TARE
+            
+            elif _system_command.type == system_command.system_command.COMMAND_TYPE_SCALE_VALUE:
+                self.last_scale_value = _system_command.value
+
+        if self.recipe_state == self.RECIPE_STATE_INITIAL_TARE:
+            # USER PLACES GLASS AND CLICKED A BUTTON 
+            if _system_command.type == system_command.system_command.COMMAND_TYPE_NAVIGATION:
+                self.tare_scale()
+                self.recipe_state = self.RECIPE_RUNNING
+            # SYSTEM WAITS FOR INCREASING LOAD ON THE SCALE AND CONTINOUES AUTOMATICALLY
+            elif _system_command.type == system_command.system_command.COMMAND_TYPE_SCALE_VALUE:
+                if _system_command.value > self.last_scale_value:
+                    self.tare_scale()
+                    self.recipe_state = self.RECIPE_RUNNING
+
+
 
 
         #if _system_command.type == system_command.system_command.COMMAND_TYPE_TIMER_IRQ:
